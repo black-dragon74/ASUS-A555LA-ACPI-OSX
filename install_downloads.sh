01d5ec6b447e12184a5c509f389906517befab8f
black.dragon74 <nickk.2974@gmail.com>
2017-12-09 01:00:28 +0530

    Install script is not WIP anymore
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- install_downloads.sh
+++ install_downloads.sh
@@ -15,9 +15,11 @@ function getFolderPath(){
 
 inputValue="$1"
 downloadFolder=$(getFolderPath $inputValue)
-targetOS="10.13" # Only use first two numbers. Like, If you are running 10.12.6 you would set targetOS="10.12" not "10.12.6"
 kFolder=$downloadFolder/kInstall
 tFolder=$downloadFolder/tInstall
+appFolder="/Applications"
+binFolder="/usr/bin"
+daeFolder="/Library/LaunchDaemons"
 
 function extractKexts(){
 	echo -e "Kexts folder found. Extracting kexts.\n"
@@ -108,6 +110,112 @@ function extractTools(){
 	echo -e "\nTools extraction completed.\n" && sleep 1
 }
 
+function installKexts(){
+	cd $kFolder
+	
+	# Deal with normal kexts
+	normallist=$(ls | grep -vE "FakeSMC_|FakePCIID_|Lilu|IntelGraphicsFix")
+	for f in $normallist;
+	do
+		sudo cp -rf $f $kextdir
+		echo Installed $f to $kextdir
+		rm -rf $f
+	done
+
+	# Deal with FakeSMC kext's plugins
+	fakesmclist=$(ls | grep "FakeSMC_")
+	for f in $fakesmclist;
+	do
+		# Ask if he wants to install that kext
+		read -p "Do you want to install $f: " fakeans
+		case $fakeans in
+			[yY]* )
+				sudo cp -rf $f $kextdir
+				;;
+			* )
+				:
+				;;	
+		esac
+		rm -rf $f
+	done
+
+	# Deal with FakePCIID's plugins
+	fakepcilist=$(ls | grep "FakePCIID_")
+	for f in $fakepcilist;
+	do
+		read -p "Do you want to install $f: " fakepcians
+		case $fakepcians in
+			[yY]* )
+				sudo cp -rf $f $kextdir
+				;;
+			* )
+				:
+				;;
+		esac
+		rm -rf $f
+	done
+
+	# Remaning kexts are to be injected by bootloader
+	# TODO: Automate bootloader injection
+	echo
+	read -p "Please copy kexts in $kFolder to EFI/CLOVER/kexts/Other and press ENTER "
+	echo
+
+	# Finished
+	echo -e "All kexts have been installed.\n"
+}
+
+function installTools(){
+	# There are three types of tools: Apps, Binaries and LaunchDaemons
+	cd $tFolder
+
+	# Take care of Applications
+	applist=$(ls | grep "app")
+	if [[ $applist != "" ]]; then
+		for f in $applist; do
+			echo "Copied $f to $appFolder"
+			sudo cp -rf $f $appFolder
+		done
+	fi
+
+	# Take care of the binaries
+	binlist=$(ls | grep -vE "app|plist")
+	if [[ $binlist != "" ]]; then
+		for f in $binlist; do
+			echo "Installed $f to $binFolder"
+			sudo cp -rf $f $binFolder
+		done
+	fi
+
+	# Take care of the LaunchDaemons
+	daelist=$(ls | grep "plist")
+	if [[ $daelist != "" ]]; then
+		for f in $daelist; do
+			echo Installed $f to $daeFolder
+			sudo cp -rf $f $daeFolder
+		done
+	fi
+
+	# Finished
+	echo -e "\nAll tools have been installed.\n"
+}
+
+function rebuildCacPer(){
+	sudo chmod -Rf 755 /L*/E*
+	sudo chown -Rf root:wheel /L*/E*
+	sudo chmod -Rf 755 /S*/L*/E*
+	sudo chown -Rf root:wheel /S*/L*/E*
+	sudo kextcache -i / &>/dev/null
+	echo -e "Done. Reboot and enjoy!"
+	exit
+}
+
+# Clear
+clear
+
+# Signature
+echo -e "Script for ASUS laptops by black.dragon74\n"
+
 if [[ -d $downloadFolder ]];
 	then
 	echo -e "Folder identified as: $downloadFolder\n"
@@ -152,3 +260,50 @@ else
 	echo "$downloadFolder is an invalid input."
 	exit 0
 fi
+
+# Now proceed for installation
+# Install kexts
+kextdir=""
+swver=$(sw_vers -productVersion | sed 's/\.//g' | colrm 5)
+
+# Determine location to install to
+if [[ $swver -ge "1011" ]]; then
+	kextdir="/Library/Extensions"
+else
+	kextdir="/System/Library/Extensions"
+fi
+
+# If 10.11 or less Lilu is not needed.
+if [[ $swver -le "1011" ]]; then
+	cd $kFolder
+
+	#For LILU
+	if [[ -e Lilu.kext ]]; then
+		rm -rf Lilu.kext
+	fi
+
+	#For IntelGraphicsFixup
+	if [[ -e IntelGraphicsFixup.kext ]]; then
+		rm -rf IntelGraphicsFixup.kext
+	fi
+fi
+
+echo -e "Proceeding to install kexts...\n"
+
+echo "Requesting superuser access..."
+if [[ $(sudo id -u 2>/dev/null) != "0" ]]; then
+	echo "Authentication failed. Aborting."
+	exit
+fi
+echo
+
+# Install kexts
+installKexts
+
+# Install tools
+echo -e "Proceeding to install tools...\n"
+installTools
+
+# Rebuild caches and fix permissions
+echo -e "Fixing permissions and rebuilding caches...\n"
+rebuildCacPer
